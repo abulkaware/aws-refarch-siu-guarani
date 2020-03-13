b39ababac541f931802ae3a36f5c92433f3dbc4f
Marcos Boaglio <mboaglio@users.noreply.github.com>
2020-03-13 14:08:01 -0300

    Update siu_install_or_run.sh

--- scripts/siu_install_or_run.sh
+++ scripts/siu_install_or_run.sh
@@ -16,15 +16,13 @@
 # $15 Nombre del bucket
 
 cd /tmp
-echo ${15}
-sudo sh -c 'aws s3 ls s3://$15/guarani.tar.gz'
 sudo sh -c 'aws s3 ls s3://${15}/guarani.tar.gz > install_s_n.txt'
 if [ -s "install_s_n.txt" ]
 then
   # El archivo existe ... lo bajo y lo instalo
   sudo aws s3 cp s3://${15}/guarani.tar.gz .
   sudo sh -c 'gunzip guarani.tar.gz'
-  sudo sh -c 'tar xvf guarani.tar /usr/local/proyectos/guarani/'
+  sudo sh -c 'tar xvf guarani.tar -C /'
 else
   # El archivo no existe ... hago una instalacion desde cero.
   ./siu_install_run_once.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15}
